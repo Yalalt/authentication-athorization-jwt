d01c4ddc168cf73d243d15dc58b3979371eef51c
Yalalt <yalaltgandush@gmail.com>
2023-03-22 23:09:53 +0800

    First commit and files created project authentication using json web token

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).json({
+      success: false,
+      message: "Хэрэглэгчийн token оруулах шаардлагатай.",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "MySuperDuperPrivateKey");
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Хэрэглэгчийн token буруу, эсвэл идэвхгүй байна.",
+    });
+  }
+  return next();
+};
+
+module.exports = verifyToken;
